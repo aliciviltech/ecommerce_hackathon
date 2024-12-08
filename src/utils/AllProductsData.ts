133d38f979d2381a6406ef22e50b8655f8529d45
aliciviltech <ali.civiltech@gmail.com>
2024-12-08 10:10:57 +0500

    firstcommit

---
+++ src/utils/AllProductsData.ts
@@ -0,0 +1,7 @@
+export const AllProductsData = [
+    {
+        id:1,
+        title:'Rocket Single Seater',
+        section:'hero'
+    }
+]
---
+++ src/utils/ProductImages.ts
@@ -0,0 +1,19 @@
+import side_table1 from '../../public/images/side_table1.png'
+import side_table2 from '../../public/images/side_table2.png'
+import outdoor_bar_table from '../../public/images/Outdoor_bar_table.png'
+import asgard_sofa from '../../public/images/asgaard_sofa.png'
+import blog1 from '../../public/images/blog1.png'
+import blog2 from '../../public/images/blog2.png'
+import blog3 from '../../public/images/blog3.png'
+import granit_dining_table from '../../public/images/granit_dining_table.png'
+import hero_rocket_single from '../../public/images/hero_rocket_single.png'
+import plain_console from '../../public/images/plain_console.png'
+import trenton_modular_sofa from '../../public/images/trenton_modular_sofa.png'
+
+export const ProductImages = {
+    side_table1:side_table1,
+    side_table2:side_table2,
+    outdoor_bar_table:outdoor_bar_table,
+    asgard_sofa:asgard_sofa,
+    blog1:blog1,
+}
