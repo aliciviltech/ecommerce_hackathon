7996df663295015b97d1728350db1cbb6bd6520a
aliciviltech <ali.civiltech@gmail.com>
2024-12-09 01:42:47 +0500

    6th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
